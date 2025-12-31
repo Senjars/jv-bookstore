dbf89c17dfae223cd0ebf7fb8819583b7b957fed
Senjars <jaroslawsendecki1@gmail.com>
2025-12-31 20:13:56 +0100

    Initial Sprring Boot project

---
+++ src/main/java/io/github/senjar/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.senjar.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/senjar/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.senjar.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
